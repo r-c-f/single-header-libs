8000be9ab05f3859f76ae9aa622f7532dd6c6bcc
Ryan Farley <ryan.farley@gmx.com>
2022-03-09 00:25:25 +0000

    add sassert.h -- emulate static_assert

---
+++ sassert.h
@@ -0,0 +1,49 @@
+/* sassert -- static assertion on as many platforms as possible
+ *
+ * Version 1.0
+ *
+ * Copyright 2022 Ryan Farley <ryan.farley@gmx.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+#pragma once
+#include <assert.h>
+
+
+/* because static_assert is guaranteed to be a macro in C11, we can avoid
+ * custom names by only defining it on lesser platforms where needed*/
+#if !defined(static_assert)
+
+/* GCC -- _Static_assert is present in >=4.6 */
+#if defined(__GNUC__) && (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#define static_assert(exp, str) _Static_assert(exp, str)
+
+/* clang -- has a nice feature to test for it */
+#elif defined(__clang__) && __has_feature(c_static_assert)
+#define static_assert(exp, str) _Static_assert(exp, str)
+
+/* Microsoft -- anything NT *should* have this
+ *
+ * Note that it does not support messages.
+*/
+#elif defined(_MSC_VER)
+#include <winnt.h>
+#define static_assert(exp, str) C_ASSERT(exp)
+
+#else
+/* treat this as an error condition for now */
+#if !defined(NDEBUG)
+#error "Static assertions not available"
+#endif /* !defined(NDEBUG) */
+#endif /* !defined(static_assert) */
+
