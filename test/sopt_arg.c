56522da4a0843209284fa5640a0a89e5c2b61e1e
Ryan Farley <ryan.farley@gmx.com>
2023-01-28 21:04:52 -0600

    sopt: support unsigned integers, characters

--- test/sopt_arg.c
+++ test/sopt_arg.c
@@ -3,10 +3,17 @@
 
 struct sopt optspec[] = {
 	SOPT_INIT_ARGL('S', "str", SOPT_ARGTYPE_STR, "str", "A string argument"),
+	SOPT_INIT_ARGL('c', "char", SOPT_ARGTYPE_CHAR, "n", "A character (numeric) argument"),
+	SOPT_INIT_ARGL('C', "schar", SOPT_ARGTYPE_SCHAR, "n", "A signed character argument"),
 	SOPT_INIT_ARGL('s', "short", SOPT_ARGTYPE_SHORT, "n", "A short argument"),
 	SOPT_INIT_ARGL('i', "int", SOPT_ARGTYPE_INT, "n", "A int argument"),
 	SOPT_INIT_ARGL('l', "long", SOPT_ARGTYPE_LONG, "n", "A long argument"),
 	SOPT_INIT_ARGL('L', "longlong", SOPT_ARGTYPE_LONGLONG, "n", "A long long argument"),
+	SOPT_INIT_ARGL(CHAR_MAX + 'C', "uchar", SOPT_ARGTYPE_UCHAR, "n", "An unsigned char argument"),
+	SOPT_INIT_ARGL(CHAR_MAX + 's', "ushort", SOPT_ARGTYPE_USHORT, "n", "An unsigned short argument"),
+	SOPT_INIT_ARGL(CHAR_MAX + 'i', "uint", SOPT_ARGTYPE_UINT, "n", "An unsigned int argument"),
+	SOPT_INIT_ARGL(CHAR_MAX + 'l', "ulong", SOPT_ARGTYPE_ULONG, "n", "An unsigned long argument"),
+	SOPT_INIT_ARGL(CHAR_MAX + 'L', "ulonglong", SOPT_ARGTYPE_ULONGLONG, "n", "An unsigned long long argument"),
 	SOPT_INIT_ARGL('f', "float", SOPT_ARGTYPE_FLOAT, "n", "A float argument"),
 	SOPT_INIT_ARGL('d', "dbl", SOPT_ARGTYPE_DBL, "n", "A double argument"),
 	SOPT_INIT_ARGL('D', "longdbl", SOPT_ARGTYPE_LONGDBL, "n", "A long double argument"),
@@ -30,6 +37,12 @@ int main(int argc, char **argv)
 			case 'S':
 				printf("Got string argument: %s\n", soptarg.str);
 				break;
+			case 'c':
+				printf("Got character argument: %d\n", soptarg.c);
+				break;
+			case 'C':
+				printf("Got signed character argument: %hhd\n", soptarg.sc);
+				break;
 			case 's':
 				printf("Got short argument: %hd\n", soptarg.s);
 				break;
@@ -42,6 +55,21 @@ int main(int argc, char **argv)
 			case 'L':
 				printf("Got long long argument: %lld\n", soptarg.ll);
 				break;
+			case CHAR_MAX + 'C':
+				printf("Got unsigned character argument: %hhu\n", soptarg.uc);
+				break;
+			case CHAR_MAX + 's':
+				printf("Got unsigned short argument: %hu\n", soptarg.us);
+				break;
+			case CHAR_MAX + 'i':
+				printf("Got unsigned int argument: %u\n", soptarg.ui);
+				break;
+			case CHAR_MAX + 'l':
+				printf("got unsigned long argument: %lu\n", soptarg.ul);
+				break;
+			case CHAR_MAX + 'L':
+				printf("Got unsigned long long argument: %llu\n", soptarg.ull);
+				break;
 			case 'f':
 				printf(float_fmt, soptarg.f);
 				break;
@@ -52,7 +80,6 @@ int main(int argc, char **argv)
 				printf(ldbl_fmt, soptarg.ld);
 				break;
 			default:
-				printf("Got unknown argument\n");
 				sopt_usage_s();
 				return 1;
 		}
